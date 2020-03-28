e47cb6d55529c8909a2df3e34747f990f5bbfe25
truong-huong <46587494+truong-huong@users.noreply.github.com>
2020-03-28 22:57:28 +0700

    Add files via upload

--- post.js
+++ post.js
@@ -124,6 +124,42 @@ var fixLanguages=(language)=> {
 // Try Catch 
 // Load same post
 try {
+  // Create table of contents
+  (function () {
+      var toc = "";
+      var level = 0;
+
+      document.getElementById("postBody").innerHTML =
+          document.getElementById("postBody").innerHTML.replace(
+              /<h([\d])>([^<]+)<\/h([\d])>/gim,
+              function (str, openLevel, titleText, closeLevel) {
+                  if (openLevel != closeLevel) {
+                      return str;
+                  }
+
+                  if (openLevel > level && level > 0) {
+                      toc += (new Array(openLevel - level + 1)).join("<ul>");
+                  } else if (openLevel < level) {
+                      toc += (new Array(level - openLevel + 1)).join("</ul>");
+                  }
+
+                  level = parseInt(openLevel);
+
+                  var anchor = titleText.replace(/ /g, "_");
+                  toc += "<li><a href=\"#" + anchor + "\">" + titleText
+                      + "</a></li>";
+
+                  return "<h" + openLevel + " id=\"" + anchor.trim() + "\">"
+                      + titleText + "</a></h" + closeLevel + ">";
+              }
+          );
+
+      if (level) {
+          toc += (new Array(level + 1)).join("</ul>");
+      }
+
+      document.getElementById("toc").innerHTML += toc;
+  })();
 Array.prototype.forEach.call(samePostArray, (el) => {
     var max_results = Math.floor(10/samePostArray.length)
    var request = new XMLHttpRequest();
@@ -176,42 +212,6 @@ Array.prototype.forEach.call(samePostArray, (el) => {
           .replace(/\<br\>/gim,'&#10;')
     });
 })(); 
-// Create table of contents
-(function () {
-    var toc = "";
-    var level = 0;
-
-    document.getElementById("postBody").innerHTML =
-        document.getElementById("postBody").innerHTML.replace(
-            /<h([\d])>([^<]+)<\/h([\d])>/gim,
-            function (str, openLevel, titleText, closeLevel) {
-                if (openLevel != closeLevel) {
-                    return str;
-                }
-
-                if (openLevel > level && level > 0) {
-                    toc += (new Array(openLevel - level + 1)).join("<ul>");
-                } else if (openLevel < level) {
-                    toc += (new Array(level - openLevel + 1)).join("</ul>");
-                }
-
-                level = parseInt(openLevel);
-
-                var anchor = titleText.replace(/ /g, "_");
-                toc += "<li><a href=\"#" + anchor + "\">" + titleText
-                    + "</a></li>";
-
-                return "<h" + openLevel + " id=\"" + anchor.trim() + "\">"
-                    + titleText + "</a></h" + closeLevel + ">";
-            }
-        );
-
-    if (level) {
-        toc += (new Array(level + 1)).join("</ul>");
-    }
-
-    document.getElementById("toc").innerHTML += toc;
-})();
 }catch(e) {}
 checkCodeLanguage();
    
