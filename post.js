e39f27f1861df0954c0d27e4f13c5147cd30783c
truong-huong <46587494+truong-huong@users.noreply.github.com>
2020-04-06 12:20:39 +0700

    Add files via upload

--- post.js
+++ post.js
@@ -111,24 +111,24 @@ var fixLanguages=(language)=> {
       var level = 0;
       document.getElementById("postBody").innerHTML =
           document.getElementById("postBody").innerHTML.replace(
-              /<h([\d])>([^<]+)<\/h([\d])>/gim,
-              function (str, openLevel, titleText, closeLevel) {
-                  if (openLevel != closeLevel) {
-                      return str;
-                  }
-                  if (openLevel > level && level > 0) {
-                      toc += (new Array(openLevel - level + 1)).join("<ul>");
-                  } else if (openLevel < level) {
-                      toc += (new Array(level - openLevel + 1)).join("</ul>");
-                  }
-                  level = parseInt(openLevel);
-                  var anchor = titleText.replace(/ /g, "_");
-                  toc += "<li><a href=\"#" + anchor + "\">" + titleText
-                      + "</a></li>";
-
-                  return "<h" + openLevel + " id=\"" + anchor.trim() + "\">"
-                      + titleText + "</a></h" + closeLevel + ">";
+            /<h([\d])>([^<]+)<\/h([\d])>/gim,
+            function (str, openLevel, titleText, closeLevel) {
+              if (openLevel != closeLevel) {
+                  return str;
               }
+              if (openLevel > level && level > 0) {
+                  toc += (new Array(openLevel - level + 1)).join("<ul>");
+              } else if (openLevel < level) {
+                  toc += (new Array(level - openLevel + 1)).join("</ul>");
+              }
+              level = parseInt(openLevel);
+              var anchor = titleText.replace(/ /g, "_");
+              toc += "<li><a href=\"#" + anchor + "\">" + titleText
+                  + "</a></li>";
+
+              return "<h" + openLevel + " id=\"" + anchor.trim() + "\">"
+                  + titleText + "</a></h" + closeLevel + ">";
+            }
           );
       (level) && (toc += (new Array(level + 1)).join("</ul>"));
       document.getElementById("toc").innerHTML += toc;
